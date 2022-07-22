77f2bcd1ab69866739c29aeeae3af3debc3cc038
milliorn <scottmilliorn@gmail.com>
2022-07-21 20:48:29 -0700

    Middle

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 export const Middle = (props) => {
+  const { dailyHigh, dailyLow, description, temp } = props;
+
   return (
     <div className="flex items-center justify-between middle">
       <div className="temperature font-semibold drop-shadow-md w-auto	tracking-tighter my-2.5	mx-0 sm:text-xl md:text-2xl">
         <p className="m-0 leading-10 capitalize weather-desc sm:text-xl md:text-2xl">
-          {props.description}
+          {description}
         </p>
         <span>Low/High</span>
         <p className="sm:text-2xl md:text-3xl">
-          {props.dailyLow}°F / {props.dailyHigh}°F
+          {dailyLow}°F / {dailyHigh}°F
         </p>
       </div>
       <p className="temperature font drop-shadow-md	text-7xl w-auto	tracking-tighter my-2.5	mx-0">
-        {props.temp}°F
+        {temp}°F
       </p>
     </div>
   );
