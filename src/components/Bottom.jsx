87b3abf8f0b0381aa14761bb8cf01e1450bcc1f4
milliorn <scottmilliorn@gmail.com>
2022-07-24 16:41:06 -0700

    Avoid multiple component definitions per file

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { toCelsius, toKph } from "../helper";
+import { Warnings } from "./Warnings";
 
 export const Bottom = (props) => {
   const {
@@ -61,46 +62,11 @@ export const Bottom = (props) => {
       );
     });
 
-  /**
-   * https://stackoverflow.com/a/20940191/11986604
-   */
-  const Warnings = () => {
-    if (Array.isArray(alert) && alert.length) {
-      const weather = alert[0];
-      const finish = new Date(weather.end * 1000);
-
-      const begin = new Date(weather.start * 1000);
-      const dateBegin = begin.toLocaleDateString();
-      const dateEnd = finish.toLocaleDateString();
-      const sender_name = weather.sender_name;
-      const tag = weather.tags[0];
-      const timeEnd = finish.toLocaleTimeString();
-      const timeStart = begin.toLocaleTimeString();
-
-      return (
-        <div className="my-4">
-          <div className="pb-2">
-            <p className="uppercase tase sm:text-2xl drop-shadow-md">
-              Warning: <span className="capitalize">{tag}</span>
-            </p>
-            <p className="sm:text-lg md:text-xl drop-shadow-md">
-              Issued by {sender_name} at {timeStart} {dateBegin} until {timeEnd}{" "}
-              {dateEnd}.
-            </p>
-            <p className="xl:text-lg 2xl:text-xl drop-shadow-md">
-              {weather.description}
-            </p>
-          </div>
-        </div>
-      );
-    }
-  };
-
   return (
     <div className="flex items-center justify-between bottom">
       <div className="w-full p-1 details">
         <BuildSections />
-        <Warnings />
+        <Warnings alert={alert} />
       </div>
     </div>
   );
