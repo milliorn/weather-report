6cd39cbe3a272e0f9d22c23aa5f6e8f0f7c0a38c
milliorn <scottmilliorn@gmail.com>
2022-07-24 23:25:28 -0700

    BuildSections

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -2,19 +2,22 @@ import React from "react";
 
 export const Top = (props) => {
   const { city, currentTime } = props;
+  const data = [city, currentTime];
+
+  const BuildSections = () =>
+    data.map((e) => {
+      return (
+        <div className="flex justify-between text-xs capitalize section-row drop-shadow-md">
+          <span className="flex justify-between text-lg font-semibold capitalize drop-shadow-md sm:text-2xl md:text-3xl section-row">
+            {e}
+          </span>
+        </div>
+      );
+    });
 
   return (
     <div className="flex items-center justify-between top">
-      <div className="flex justify-between text-xs capitalize section-row drop-shadow-md">
-        <span className="flex justify-between text-lg font-semibold capitalize drop-shadow-md sm:text-2xl md:text-3xl section-row">
-          {city}
-        </span>
-      </div>
-      <div className="flex justify-between text-xs capitalize section-row drop-shadow-md">
-        <span className="flex justify-between text-lg font-semibold capitalize drop-shadow-md sm:text-2xl md:text-3xl section-row">
-          {currentTime}
-        </span>
-      </div>
+      <BuildSections />
     </div>
   );
 };
