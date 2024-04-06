1033174f82d1f796cf55f761d5c343c906eaafbd
milliorn <scottmilliorn@gmail.com>
2024-04-06 15:50:44 -0700

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,90 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+/**
+ * Represents the metadata for the Weather Report Application.
+ *  https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+ */
 export const metadata: Metadata = {
-  title: "Weather Report",
+  /**
+   * The name of the application.
+   */
+  applicationName: "Weather Report Application",
+
+  /**
+   * The creator of the application.
+   */
+  creator: "Scott Milliorn",
+
+  /**
+   * The description of the application.
+   */
   description: "Get the latest weather report for your location.",
+
+  /**
+   * The keywords associated with the application.
+   */
+  keywords: ["weather", "report", "forecast"],
+
+  /**
+   * The robots meta tag for search engine indexing.
+   */
+  robots: "index, follow",
+
+  /**
+   * The title of the application.
+   */
+  title: "Weather Report",
+
+  /**
+   * The publisher of the application.
+   */
+  publisher: "Scott Milliorn",
+
+  /**
+   * The Twitter metadata for the application.
+   */
+  twitter: {
+    /**
+     * The Twitter site handle.
+     */
+    site: "@scottmilliorn",
+
+    /**
+     * The Twitter creator handle.
+     */
+    creator: "@scottmilliorn",
+  },
+
+  /**
+   * The format detection settings for the application.
+   */
+  formatDetection: {
+    /**
+     * Whether email format detection is enabled.
+     */
+    email: false,
+
+    /**
+     * Whether address format detection is enabled.
+     */
+    address: false,
+
+    /**
+     * Whether telephone format detection is enabled.
+     */
+    telephone: false,
+  },
+
+  /**
+   * The authors of the application.
+   */
+  authors: [{ name: "Scott Milliorn", url: "https://milliorn.xyz" }],
+
+  /**
+   * The generator of the application.
+   */
+  generator: "Next.js",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
-
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
         Welcome to <span className="text-blue-600">Tailwind CSS</span>
       </h1>
-      <Image
-        src="/tailwindcss-logo.svg"
-        alt="Tailwind CSS Logo"
-        width={300}
-        height={300}
-      />
     </main>
   );
 }
