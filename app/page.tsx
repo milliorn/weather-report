2460f770c220a8a10feed325e64e2e3bf4f9a6fd
milliorn <scottmilliorn@gmail.com>
2024-04-08 22:47:37 -0700

    add return type

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
