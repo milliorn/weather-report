2820118caf4c63bfeb87fa2191e808b3478fd322
milliorn <scottmilliorn@gmail.com>
2024-04-10 22:04:04 -0700

    npm install react-select react-select-async-paginate

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,45 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
 import CurrentWeather from "./components/CurrentWeather";
-import Search from "./components/Search";
-import { useState } from "react";
+
+const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const GeoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_X_RAPID_KEY,
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
 
 export default function Home(): JSX.Element {
   const [currentWeather, setCurrentWeather] = useState(null);
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const fetchResponse = await fetch(
+        `${GEO_API_URL}/cities?offset=0&minPopulation=1&sort=-population&namePrefix=${inputValue}`,
+        GeoApiOptions
+      );
+      const response = await fetchResponse.json();
+      return {
+        options: response.data.map((city) => {
+          return {
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name}, ${city.country}`,
+          };
+        }),
+      };
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const handleOnSearchChange = (searchData) => {
+    setSearch(searchData);
     const [latitude, longitude] = searchData.value.split(" ");
-
     const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-
     const currentWeatherFetch = fetch(
       `${WEATHER_API_URL}/onecall?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=imperial`
     );
@@ -31,7 +61,16 @@ export default function Home(): JSX.Element {
         backgroundImage: imageUrl,
       }}
     >
-      <Search onSearchChange={handleOnSearchChange} />
+      <label>
+        <AsyncPaginate
+          id="searchbar"
+          placeholder="Click here and type city name."
+          debounceTimeout={600}
+          value={search}
+          onChange={handleOnSearchChange}
+          loadOptions={loadOptions}
+        />
+      </label>
       {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
