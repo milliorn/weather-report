a2ba575bfbc7eee5b2732f2eb11380b1d6adfc1e
milliorn <scottmilliorn@gmail.com>
2024-04-10 22:11:37 -0700

    added types

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,16 @@ const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const GeoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_X_RAPID_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_X_RAPID_KEY || "",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export default function Home(): JSX.Element {
-  const [currentWeather, setCurrentWeather] = useState(null);
-  const [search, setSearch] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState<any>(null);
+  const [search, setSearch] = useState<any>(null);
 
-  const loadOptions = async (inputValue) => {
+  const loadOptions = async (inputValue: string) => {
     try {
       const fetchResponse = await fetch(
         `${GEO_API_URL}/cities?offset=0&minPopulation=1&sort=-population&namePrefix=${inputValue}`,
@@ -24,7 +24,7 @@ export default function Home(): JSX.Element {
       );
       const response = await fetchResponse.json();
       return {
-        options: response.data.map((city) => {
+        options: response.data.map((city: any) => {
           return {
             value: `${city.latitude} ${city.longitude}`,
             label: `${city.name}, ${city.country}`,
@@ -32,11 +32,14 @@ export default function Home(): JSX.Element {
         }),
       };
     } catch (err) {
-      console.error(err);
+      // eslint-disable-next-line no-console
+      console.error(`Error fetching data: ${err}`);
+      return { options: [] };
     }
   };
 
-  const handleOnSearchChange = (searchData) => {
+  const handleOnSearchChange = (searchData: any) => {
+    // Change the type of searchData to any
     setSearch(searchData);
     const [latitude, longitude] = searchData.value.split(" ");
     const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
@@ -49,6 +52,7 @@ export default function Home(): JSX.Element {
         const weatherResponse = await response[0].json();
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
       })
+      // eslint-disable-next-line no-console
       .catch(console.warn);
   };
 
