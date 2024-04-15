24c19c5f30362566dc6d9b2ff896d50ca610c9d4
milliorn <scottmilliorn@gmail.com>
2024-04-14 22:06:23 -0700

    Remove unused CSS files and update import paths in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/css/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
