451c0b2706b41eaaa77e0f35a9d830fe47d5abbc
milliorn <scottmilliorn@gmail.com>
2024-04-20 14:06:18 -0700

    Add constants file with GEO_API_URL, HTTP_INTERNAL_SERVER_ERROR, and HTTP_OK

---
+++ pages/api/constants.ts
@@ -0,0 +1,5 @@
+const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+const HTTP_INTERNAL_SERVER_ERROR = 500;
+const HTTP_OK = 200;
+
+export { GEO_API_URL, HTTP_INTERNAL_SERVER_ERROR, HTTP_OK };
