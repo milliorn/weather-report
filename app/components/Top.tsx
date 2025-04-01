264dc2adefb83b3adc3d11dcb072df5d13b52c88
milliorn <scottmilliorn@gmail.com>
2025-03-31 22:43:45 -0700

    refactor Top.tsx

--- app/components/Top.tsx
+++ app/components/Top.tsx
@@ -5,28 +5,29 @@ import { TopProps } from "../models/componentProps";
 /**
  * Renders the top section of the page.
  *
- * @component
- * @param {Object} props - The component props.
+ * Displays the city name and current time.
+ *
+ * @param {TopProps} props - The component props.
  * @param {string} props.city - The city name.
  * @param {string} props.currentTime - The current time.
  * @returns {JSX.Element} The rendered top section.
  */
 const Top = (props: TopProps): JSX.Element => {
+  // Destructure props for clarity
   const { city, currentTime } = props;
-  const data = [city, currentTime];
 
   return (
     <div className="flex items-center justify-between top">
-      {data.map((item, index) => (
-        <div
-          key={`${index}_${item}`}
-          className="flex justify-between text-xs capitalize section-row drop-shadow-md"
-        >
-          <span className="text-lg font-semibold capitalize drop-shadow-md sm:text-2xl md:text-3xl">
-            {item}
-          </span>
-        </div>
-      ))}
+      <div className="flex justify-between text-xs capitalize section-row drop-shadow-md">
+        <span className="text-lg font-semibold capitalize drop-shadow-md sm:text-2xl md:text-3xl">
+          {city}
+        </span>
+      </div>
+      <div className="flex justify-between text-xs capitalize section-row drop-shadow-md">
+        <span className="text-lg font-semibold capitalize drop-shadow-md sm:text-2xl md:text-3xl">
+          {currentTime}
+        </span>
+      </div>
     </div>
   );
 };
